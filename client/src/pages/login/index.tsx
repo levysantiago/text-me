3dd57638a99e3d80d214df77e845881947d1f5f6
Levy Santiago <levyssantiago@gmail.com>
2023-04-20 23:13:06 -0300

    feat: login page requesting server

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import {
   ButtonContainer,
   InputsTitle,
@@ -10,19 +10,24 @@ import {
 import { Input } from 'components/Input'
 import { DefaultButton } from 'components/buttons/DefaultButton'
 import { WebsiteContainer } from 'templates/WebsiteContainer'
+import { loginService } from 'services/loginService'
+import { AppContext } from 'components/context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
-export default async function Login() {
+export default function Login() {
+  const { setAccessToken } = useContext(AppContext)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const navigate = useNavigate()
 
   async function handleSubmit() {
-    // appCookies.set('access_token', 'asd')
-    // console.log(appCookies)
-    // console.log(setAccessToken)
-    // console.log(accessToken)
-    // const responseData = await loginService({ email, password })
-    // console.log(responseData)
-    // setAccessToken(responseData.data.access_token)
+    try {
+      const responseData = await loginService({ email, password })
+      setAccessToken(responseData.data.access_token)
+      navigate('/')
+    } catch (e: any) {
+      console.log(e.response.data)
+    }
   }
 
   return (
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Chat from './pages/Chat'
 import Home from './pages/Home'
+import Login from 'pages/Login'
 
 export const MyRoutes = (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/chat" element={<Chat />} />
     </Routes>
   </BrowserRouter>
