fe1f82d8670ec51a895690a55afe06b378b8c933
Levy Santiago <levyssantiago@gmail.com>
2024-10-02 16:48:54 -0300

    test: get friends messages resume service

--- server/src/modules/chat/services/get-friends-messages-resume.service.ts
+++ server/src/modules/chat/services/get-friends-messages-resume.service.ts
@@ -65,8 +65,10 @@ export class GetFriendsMessagesResumeService {
 
       return { data };
     } catch (err) {
-      console.log(err);
-      throw new MessagesNotFoundError();
+      // console.log(err);
+      throw new MessagesNotFoundError({
+        reason: err.message,
+      });
     }
   }
 }
