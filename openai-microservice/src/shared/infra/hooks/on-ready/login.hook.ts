e515b2da8163e519425179cef508ea9bb97ad594
Levy Santiago <levyssantiago@gmail.com>
2024-10-09 11:46:11 -0300

    test: login hook

--- openai-microservice/src/shared/infra/hooks/on-ready/login.hook.ts
+++ openai-microservice/src/shared/infra/hooks/on-ready/login.hook.ts
@@ -5,34 +5,27 @@ import { textmeServer } from '@shared/resources/api/textme-server'
 import { env } from '@shared/resources/env'
 
 export async function loginHook(): Promise<void> {
-  try {
-    const cacheProvider = container.resolve<ICacheProvider>('CacheProvider')
+  const cacheProvider = container.resolve<ICacheProvider>('CacheProvider')
 
-    const response = await textmeServer.post('/auth', {
-      email: env.USER_EMAIL,
-      password: env.USER_PASSWORD,
-    })
+  const response = await textmeServer.post('/auth', {
+    email: env.USER_EMAIL,
+    password: env.USER_PASSWORD,
+  })
 
-    if (response.status !== 200) {
-      throw new Error('Failed to sign in')
-    }
-
-    // Validating response data
-    const responseDataSchema = z.object({
-      data: z.object({ access_token: z.string() }),
-    })
-
-    // Parsing response data
-    const responseData = responseDataSchema.safeParse(response.data)
-    if (!responseData.success) {
-      throw new Error('Invalid server response data')
-    }
-
-    await cacheProvider.save(
-      'access_token',
-      responseData.data.data.access_token,
-    )
-  } catch (err) {
-    console.log(err)
+  if (response.status !== 200) {
+    throw new Error('Failed to sign in')
   }
+
+  // Validating response data
+  const responseDataSchema = z.object({
+    data: z.object({ access_token: z.string() }),
+  })
+
+  // Parsing response data
+  const responseData = responseDataSchema.safeParse(response.data)
+  if (!responseData.success) {
+    throw new Error('Invalid server response data')
+  }
+
+  await cacheProvider.save('access_token', responseData.data.data.access_token)
 }
