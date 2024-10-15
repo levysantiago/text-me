83e4e4cc2ce6b6fd86fb1288217a122be4741d90
Levy Santiago <levyssantiago@gmail.com>
2024-10-15 09:50:32 -0300

    docs: heartbeat controller

--- server/src/modules/heartbeat/infra/http/controllers/heartbeat.controller.ts
+++ server/src/modules/heartbeat/infra/http/controllers/heartbeat.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AppErrorDTO } from '@shared/resources/errors/dtos/app-error-dto';
 import { Response } from 'express';
 
 @Controller('/')
 @ApiTags("Heartbeat")
 export class HeartbeatController {
   @Get('/')
+  @ApiOkResponse({
+    example: {data: "TextMe Server is UP!"}
+  })
+  @ApiResponse({
+    type: AppErrorDTO,
+    status: 500
+  })
   async handle(@Res() response: Response) {
     return response.status(200).json({ data: 'TextMe Server is UP!' });
   }
