0490dff2dfc4d6c95ed6c8b079b4424a7be6b1d5
Levy Santiago <levyssantiago@gmail.com>
2024-10-15 15:13:45 -0300

    docs: check login

--- server/src/modules/auth/infra/http/controllers/check-login.controller.ts
+++ server/src/modules/auth/infra/http/controllers/check-login.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Res, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ApiGlobalHeaders } from '@shared/infra/http/decorators/api-global-headers.decorator';
 import { JwtAuthGuard } from '@shared/infra/http/guards/jwt-auth.guard';
+import { AppErrorDTO } from '@shared/resources/errors/dtos/app-error-dto';
 import { Response } from 'express';
 
 @Controller('api')
@@ -11,6 +12,25 @@ import { Response } from 'express';
 export class CheckLoginController {
   @Get('auth/check')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: "Checks if user is logged.",
+  })
+  @ApiOkResponse({
+    description: "Valid response.",
+  })
+  @ApiResponse({
+    type: AppErrorDTO,
+    example: {
+      statusCode: 401,
+      error: "UNAUTHORIZED",
+      message: "Unauthorized",
+      reason: "",
+      timestamp: "2024-10-14T19:06:52.956Z",
+      path: "api/auth/check",
+    } as AppErrorDTO,
+    description: "App Error. If user not logged it will throw 'UNAUTHORIZED' with status 401.",
+    status: 500
+  })
   async handle(@Res() response: Response) {
     response.status(200).send();
   }
--- server/src/shared/infra/http/guards/jwt-auth.guard.ts
+++ server/src/shared/infra/http/guards/jwt-auth.guard.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { UnauthorizedError } from '@shared/infra/errors/unauthorized.error';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
+    if(err || !user){
+      throw new UnauthorizedError();
+    }
+
+    return user;
+  }
+}
