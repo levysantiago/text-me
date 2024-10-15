dac3b3b01298d3f604f4ef473e8119f90704311b
Levy Santiago <levyssantiago@gmail.com>
2024-10-15 17:40:02 -0300

    chore: formatting main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@shared/infra/filters/exeption.filter';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,19 +9,21 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const docsConfig = new DocumentBuilder()
-    .setTitle("TextMe Server Docs")
-    .setDescription("This is the TextMe application Server which is responsible for handling all users, friendships and chat operations.")
-    .setVersion("1.0")
+    .setTitle('TextMe Server Docs')
+    .setDescription(
+      'This is the TextMe application Server which is responsible for handling all users, friendships and chat operations.',
+    )
+    .setVersion('1.0.0')
     .addBearerAuth()
-    .build()
-    
+    .build();
+
   app.enableCors({
     credentials: true,
     origin: 'http://localhost:3000',
   });
 
-  const document = SwaggerModule.createDocument(app, docsConfig)
-  SwaggerModule.setup("docs", app, document)
+  const document = SwaggerModule.createDocument(app, docsConfig);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3333);
 }
